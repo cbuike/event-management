d7cef2887ddd39bfc61d63d5a23725e8f021dc84
cbuike <offxtiancee@gmail.com>
2025-12-14 12:26:34 +0100

    added Swagger API definition, modified Dto and docker compose container name

--- src/main/java/com/eventmanagement/EventManagementApplication.java
+++ src/main/java/com/eventmanagement/EventManagementApplication.java
@@ -1,8 +1,27 @@
 package com.eventmanagement;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Event Category Management Application",
+                version = "v1.0",
+                description = "API for Event Category Management Application [by Chibuike Okeke]",
+                contact = @Contact(
+                        name = "Chibuike Okeke",
+                        email = "offxtiancee@gmail.com"
+                ),
+                license = @License(
+                        name = "Apache 2.0",
+                        url = "https://github.com/cbuike"
+                )
+        )
+)
 @SpringBootApplication
 public class EventManagementApplication {
 
--- src/main/java/com/eventmanagement/dto/MoveRequest.java
+++ src/main/java/com/eventmanagement/dto/MoveRequest.java
@@ -1,10 +1,12 @@
 package com.eventmanagement.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Data;
 
 @Builder
 @Data
 public class MoveRequest {
+    @Schema(example = "3")
     public Long newParentId;
 }
